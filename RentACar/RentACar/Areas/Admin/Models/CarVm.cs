ba4284101281976c0130fb049190f5e13bfdd667
Berk Ersoy <berkersoylar@outlook.com>
2025-09-13 22:23:53 +0300

    Created Areas/Admin

---
+++ RentACar/RentACar/Areas/Admin/Models/CarVm.cs
@@ -0,0 +1,17 @@
+﻿namespace RentACar.Areas.Admin.Models
+{
+    public class CarVm
+    {
+        public int CarId { get; set; }
+        public string CarName { get; set; }
+        public decimal CarPrice { get; set; }
+        public decimal? CampaignPrice { get; set; }
+        public bool IsCampaignAvailable { get; set; }
+
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+
+        public List<string> ImageUrls { get; set; } = new List<string>();
+        public decimal DisplayPrice => IsCampaignAvailable && CampaignPrice.HasValue ? CampaignPrice.Value : CarPrice;
+    }
+}
